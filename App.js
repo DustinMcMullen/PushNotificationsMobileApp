d5985ab77572c1ec0e41cddf3962a385a36891ae
Dustin McMullen <mcmullendustin@yahoo.com>
2022-11-01 01:19:25 -0500

    Added push notification functionality & handling

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View, Platform } from 'react-native';
 
 import * as Notifications from "expo-notifications";
 
@@ -18,19 +18,46 @@ Notifications.setNotificationHandler({
 export default function App() {
 
   useEffect(() => {
+    async function configPushNotifications() {
+      const {status} = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+      if (finalStatus !== "granted") {
+        const {status} = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        Alert.alert(
+          "Permissions Required",
+          "Push Notifications must be enabled to allow notifications."
+        );
+        return;
+      }
+      const pushTokenData = await Notifications.getExpoPushTokenAsync();
+
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.DEFAULT
+        });
+      }
+    }
+
+    configPushNotifications();
+
+  }, []);
+
+  useEffect(() => {
     const subscriptionRec = Notifications.addNotificationReceivedListener((notification) => {
       const userName = notification.request.content.data.userName;
-      console.log("userName: ", userName);
     });
 
     const subscriptionRes = Notifications.addNotificationResponseReceivedListener((response) => {
-      console.log("Notification Response: ", response);
     });
 
     // This return function is useEffect best practice
     // It will automatically execute for cleanup when useEffect is run again or parent component is removed.
     return () => {
-      // .remove is a method on the "subsbriction" promise returned by the listeners. It is used to cleanup event listener on removal of event. 
+      // .remove() is a method on the "subsbriction" promise returned by the listeners. It is used to cleanup event listener on removal of event. 
       subscriptionRec.remove();
       subscriptionRes.remove();
     }
@@ -49,13 +76,31 @@ export default function App() {
     });
   }
 
+  function sendPushNotificationHandler() {
+    fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to: "ExponentPushToken[9ZM7ZUB4SCC1SX5k-Esyd3]",
+        title: "Test sent from device",
+        body: "This is a test push notification sent from mobile device."
+      })
+    })
+  }
+
   return (
     <View style={styles.container}>
-    <Button
-      title="Schedule Notification"
-      onPress={scheduleNotificationHandler}
-    />
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
       <Text>Hello World!!!!</Text>
+      <Button
+        title="Send Push Notification"
+        onPress={sendPushNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
